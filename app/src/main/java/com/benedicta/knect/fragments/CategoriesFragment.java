743f00946ad8bd7918b9c2634904500e3756b857
nutifafafiala <peace1fafa@gmail.com>
2020-02-26 13:54:31 +0000

    update

---
+++ app/src/main/java/com/benedicta/knect/fragments/CategoriesFragment.java
@@ -0,0 +1,26 @@
+package com.benedicta.knect.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.benedicta.knect.R;
+
+public class CategoriesFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+
+    }
+}
